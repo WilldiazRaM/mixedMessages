4d2fbc3b1772122a76914ab906a5d56db597dd98
WilldiazRaM <wi.diaz@duocuc.cl>
2023-11-20 12:16:20 -0300

    agregado const diaSemana y menjaejsMotivacionales

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 /* testing git */
+const diaSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'
+];
+
+const mensajesMotivacionales = [
+  "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
+  "Cada día es una nueva oportunidad para cambiar tu vida.",
+  "La única forma de hacer un gran trabajo es amar lo que haces.",
+  "No te preocupes por los fracasos, preocúpate por las posibilidades que pierdes cuando ni siquiera lo intentas.",
+  "La perseverancia no es una carrera larga, sino muchos sprints cortos, uno tras otro.",
+  "El único lugar donde el éxito viene antes que el trabajo es en el diccionario.",
+  "No importa lo lento que vayas, siempre y cuando no te detengas.",
+  "Cree en ti mismo y todo será posible.",
+  "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito. Si amas lo que estás haciendo, serás exitoso.",
+  "Nunca es demasiado tarde para ser lo que podrías haber sido."
+];
+
+
+
 const funcion1 = arr => {
     
 }
